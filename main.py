9a09e3bf7498c2638e46cb5505b5be19f3f8f2d9
Tom Mong <mongyin98@gmail.com>
2022-02-24 05:40:22 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from auvsi_suas.client import client
+from auvsi_suas.proto import interop_api_pb2
+
+client = client.Client(url='http://hkuuas-suas.mysgapp.co',
+                       username='testuser',
+                       password='testpass')
+
+mission = client.get_mission(1)
+print(mission)
